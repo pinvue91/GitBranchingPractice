9e6ecd55cadb158f5f79ff118e06c95083d75449
pinvue91 <pin.vue@hotmail.com>
2020-10-29 11:02:09 -0400

    more edits

--- GitBranchingPractice/Program.cs
+++ GitBranchingPractice/Program.cs
@@ -7,10 +7,10 @@ namespace GitBranchingPractice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            string[] names = { "Joey", "Juan", "Raymundo", "Stacy" };
+            string[] names = { "Joey", "Juan", "Raymundo", "Rema" };
             foreach(string name in names)
             {
-                Console.WriteLine(name + "is a teacher.");
+                Console.WriteLine(name + "is a a principal.");
             }
         }
     }
