25336c68802e7207d9cc7a635781431904442994
pinvue91 <pin.vue@hotmail.com>
2020-10-29 10:33:36 -0400

    Practicing Branching

---
+++ GitBranchingPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GitBranchingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] names = { "Joey", "Juan", "Raymundo", "Lucy" };
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
